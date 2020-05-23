62a0cb572e16004e52679b28d6d116f13fb737e4
Gaurav Vaidya <gaurav.vaidya1@gmail>
2020-05-23 12:56:42 -0500

    Refactored Code
    Cleaner code with some Python best practices

--- GetNICVendor.py
+++ GetNICVendor.py
@@ -3,33 +3,55 @@ import requests
 import sys
 import re
 
-#in case user does not provide a cmd arg
-if (len(sys.argv) == 2):
-    #capture command line argument and validate that it is a MAC ADDRESS
-    macAddress = sys.argv[1]
-    if re.match('([a-fA-F0-9]{2}[:|\-]?){6}$', macAddress):
-        #mac address api-endpoint
-        API_ENDPOINT = 'https://api.macaddress.io/v1'
+#calls mac address api to get vendor name and return it
+def retrieveNICVendor(macAddress):
+    #mac address api-endpoint
+    API_ENDPOINT = 'https://api.macaddress.io/v1'
 
-        ##input parameters for get request:
-        #My API Key for macaddress api
-        APIKEY = "at_z7i9dbzdYrwBMSWoXpgtVz6cSdHmR"
+    ##input parameters for get request:
+    #My API Key for macaddress api. probably should hide this somehow
+    APIKEY = "at_z7i9dbzdYrwBMSWoXpgtVz6cSdHmR"
 
-        #Output format. To meet requirements, initializing as 'vendor'.
-        #Although, can change to 'json' and parse more information from API call
-        OUTPUTFORMAT = "vendor"
+    #Output format. To meet requirements, initializing as 'vendor'.
+    #Although, can change to 'json' and parse more information from API call
+    OUTPUTFORMAT = "vendor"
 
-        #PARAMS dictionary as parameters sent to API
-        PARAMS = {
-            'apiKey': APIKEY,
-            'search': macAddress,
-            'output': OUTPUTFORMAT
-        }
-        response = requests.get(API_ENDPOINT, params=PARAMS)
-        print(response.text)
+    PARAMS = {
+        'apiKey': APIKEY,
+        'search': macAddress,
+        'output': OUTPUTFORMAT
+    }
+    response = requests.get(API_ENDPOINT, params=PARAMS)
+    return response.text
+
+#determines if passed in string is in valid mac address format
+def isValidMacAddress(x):
+    #return true if parameter matches regex for mac addresses
+    if re.match('([a-fA-F0-9]{2}[:|\-]?){6}$', x):
+        return True
     else:
-        #user did not provide a valid cmd arg
-        print("MAC address is not in valid format. Please input a MAC address without spaces as command line argument")
-else:
-    #user did not provide a valid cmd arg
-    print("Please input a MAC address without spaces as command line argument")
+        return False
+
+#gets the command line argument only if there is one. else returns None
+def getMacAddressFromCMD():
+    #enforces exactly 1 command line argument
+    if (len(sys.argv) == 2):
+        #capture command line argument and return
+        return sys.argv[1]
+    else:
+        return None
+
+#main driver code
+def main():
+    macAddress = getMacAddressFromCMD()
+    if(macAddress is None):
+        print("Please input a MAC address without spaces as command line argument")
+    else:
+        if(isValidMacAddress(macAddress)):
+            print(retrieveNICVendor(macAddress))
+        else:
+            print("MAC address is not in valid format. Please input a MAC address without spaces as command line argument")
+
+
+if __name__ == "__main__":
+    main()
